1017080c10031ecea4649b8d83679e51d10357d0
Leonardo Gomes <elwgomes@icloud.com>
2023-01-09 11:04:08 -0300

    add: delete data by id

--- src/main/java/com/elwgomes/springbootthymeleafajax/resources/EmployeeResource.java
+++ src/main/java/com/elwgomes/springbootthymeleafajax/resources/EmployeeResource.java
@@ -41,7 +41,7 @@ public class EmployeeResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/delete/{id}")
     public ResponseEntity<Void> delete (@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
